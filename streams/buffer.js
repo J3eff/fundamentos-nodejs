2d7c4c1b34f7afc07274f83f24d1f2aa3c31c710
J3eff <jeffersonvideo125@gmail.com>
2025-09-07 17:23:19 -0300

    note: Streams no Node.js - Entendendo Buffers no Node.js

---
+++ streams/buffer.js
@@ -0,0 +1,6 @@
+// buffer -> Representa um espaço na memória onde podemos armazenar dados
+// streams -> Permite trabalhar com fluxos de dados (leitura e escrita de forma contínua)
+
+const buf = Buffer.from('ok');
+
+console.log(buf.toJSON());

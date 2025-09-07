76bc193f066e5c8c71a792ac6538784d34ee5714
J3eff <jeffersonvideo125@gmail.com>
2025-09-07 17:48:38 -0300

    feat: Persistindo banco de dados

--- src/database.js
+++ src/database.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+
+const databaePath = new URL('../db.json', import.meta.url);
+
 export class Database {
     #database = {}
+
+    constructor() {
+        fs.readFile(databaePath, 'utf-8').then(data => {
+            this.#database = JSON.parse(data);
+        }).catch(() => {
+            this.#persist();
+        })
+    }
+
+    #persist() {
+        fs.writeFile(databaePath, JSON.stringify(this.#database));
+    }
 
     select(table) {
         return this.#database[table] ?? [];
@@ -11,6 +27,8 @@ export class Database {
         else {
             this.#database[table] = [data];
         }
+
+        this.#persist();
 
         return data;
     }
